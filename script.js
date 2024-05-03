fe01218e39ea2b73f4a059407597a9fbf2fb550e
Gerth123 <robingerth21@gmail.com>
2024-05-03 22:13:58 +0200

    Added the baseUrl for the remotestorage and the functions to load post delete and put data. The Init funtion will be used for the initialisation.

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let baseUrl = 'https://join-ca44d-default-rtdb.europe-west1.firebasedatabase.app/';
+
+async function loadData(path='') {
+    let response = await fetch(baseUrl + path + '.json');
+    return responseToJson = await response.json();
+}
+
+async function postData(path='', data={}) {
+    let response = await fetch(baseUrl + path + '.json', {
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    return responseToJson = await response.json();
+}
+
+// postData('', {'name':'alice'});
+
+async function deleteData(path='') {
+    let response = await fetch(baseUrl + path + '.json', {
+        method: 'DELETE',
+    });
+    return responseToJson = await response.json();
+}
+
+async function putData(path='', data={}) {
+    let response = await fetch(baseUrl + path + '.json', {
+        method: 'PUT',
+        header: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    return responseToJson = await response.json();
+}
+
+// function init() {
+//     putData('', {'name':'xy'});
+// }
+
 function openNewTab(newUrl) {
     newWindow = window.open(newUrl, '_blank');
 }
