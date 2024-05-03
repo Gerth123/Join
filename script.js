7faecf0b38b4c43bcaad690e3c627e42aeb74375
Gerth123 <robingerth21@gmail.com>
2024-05-03 19:40:53 +0200

    Added the openNewTab function for legal_notice and privacy_policy. Also added the goBackToLastTab function for legal_notice and privacy_policy with onclick on the blue leftsided arrow.

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// let previousWindow = [];
-
-// function openNewTab(newUrl, previousUrl) {
-//     window.open(newUrl, '_blank');
-//     previousWindow.splice(0, 1);
-//     previousWindow.push(previousUrl);
-// }
-// function goBackToLastTab() {
-//     window.open('add_task.html');
-// }
-
-let previousWindow = null;
-
-// Funktion zum Öffnen eines neuen Tabs und Speichern der Referenz zum vorherigen Tab
-function openNewTab(newUrl, previousUrl) {
-    previousWindow = previousUrl;
+function openNewTab(newUrl) {
     newWindow = window.open(newUrl, '_blank');
 }
 
 // Funktion zum Schließen des neuen Tabs und Zurückkehren zum vorherigen Tab
 function goBackToLastTab() {
-    window.location.href = previousWindow;
+    window.close();
 }
