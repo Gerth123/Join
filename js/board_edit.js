c5ec7f5de1bdbc95eb038ef6ffed11177fd4c1cd
hanbit10 <chanbit10@gmail.com>
2024-05-15 14:58:04 +0200

    Board: update

--- js/board_edit.js
+++ js/board_edit.js
@@ -107,7 +107,6 @@ let show = true;
 
 function showCheckboxes() {
   let checkboxes = document.getElementById("checkBoxes");
-
   if (show) {
     checkboxes.style.display = "block";
     show = false;
