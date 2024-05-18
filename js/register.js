aabf79e001cd7844d34df5818308e13cd61e6314
Gerth123 <robingerth21@gmail.com>
2024-05-18 14:28:31 +0200

    Did some cleancoding JSDOC and added a function that checks the name if its the full name at the register field.

--- js/register.js
+++ js/register.js
@@ -199,44 +199,100 @@ function generateRandomColor() {
     return color;
 }
 
-
-//Math.floor(Math.random() * 10000);
-
+/**
+ * This function is used to add a new user to the database.
+ * 
+ * @author: Robin
+ */
 async function addUser() {
     let msgBox = document.getElementById('msgBox');
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
     let password = document.getElementById('password1');
     let confirmPassword = document.getElementById('password2');
-    let newArray = [{ 'mail': email.value, 'name': name.value, 'password': password.value, 'contacts': testContacts, 'tasks': testTasks }];
+    let newArray = [{ 'mail': email, 'name': name, 'password': password.value, 'contacts': testContacts, 'tasks': testTasks }];
     let actualUsers = await loadData('users');
     let actualisedUsers = [...newArray, ...actualUsers];
     if (actualUsers.length === 0) {
         actualisedUsers = [...newArray];
-    };
-    for (let mailSearchIndex = 0; mailSearchIndex < actualUsers.length; mailSearchIndex++) {
-        if (actualUsers[mailSearchIndex] === null) {
-            continue;
-        } else {
-            let mailToCheck = actualUsers[mailSearchIndex]['mail'];
-            if (JSON.stringify(mailToCheck) === JSON.stringify(email.value)) {
-                let msgBoxText = document.getElementById('msgBoxText');
-                msgBoxText.innerHTML = 'User already exists. Please use another Mail-Adress!';
-                msgBox.classList.remove('d-none');
-                await setTimeout(function () {
-                    msgBox.classList.add('d-none');
-                }, 2000);
-                return;
-            }
-        }
-    };
+    }
+    let mailExists = await checkMail(msgBox, email, actualUsers);
+    if (mailExists) {
+        return;
+    }
+    let nameChecked = await checkName(msgBox, name);
+    if (!nameChecked) {
+        return;
+    }
     await putNewUser(password, confirmPassword, msgBox, actualisedUsers);
 }
 
+/**
+ * This function checks if the user mail already exists in the database.
+ * 
+ * @param {Element} msgBox - The message box element to show if the mail already exists.
+ * @param {string} email - The email to check.
+ * @param {Array} actualUsers - The array of actual users.
+ * 
+ * @returns {boolean} - Returns true if the email exists, otherwise false.
+ * @author: Robin
+ */
+async function checkMail(msgBox, email, actualUsers) {
+    for (let user of actualUsers) {
+        if (user && user.mail === email) {
+            let msgBoxText = document.getElementById('msgBoxText');
+            msgBoxText.innerHTML = 'User already exists. Please use another Mail-Adress!';
+            msgBox.classList.remove('d-none');
+            await new Promise(resolve => setTimeout(() => {
+                msgBox.classList.add('d-none');
+                resolve();
+            }, 2000));
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * This function checks if the user name is valid.
+ * 
+ * @param {Element} msgBox - The message box element to show if the name is invalid.
+ * @param {string} fullName - The full name of the user.
+ * 
+ * @returns {boolean} - Returns true if the name is valid, otherwise false.
+ * @author: Robin
+ */
+async function checkName(msgBox, fullName) {
+    let nameParts = fullName.trim().split(' ');
+    if (nameParts.length !== 2) {
+        let msgBoxText = document.getElementById('msgBoxText');
+        msgBoxText.innerHTML = 'Please enter your full name.';
+        msgBox.classList.remove('d-none');
+        await new Promise(resolve => setTimeout(() => {
+            msgBox.classList.add('d-none');
+            resolve();
+        }, 2000));
+        return false;
+    }
+    return true;
+}
+
+/**
+ * This function checks if the user password and confirm password are the same and shows a message if they are not.
+ * If the password and confirm password are the same, the user is added to the database and gets to the summary page.
+ * 
+ * @param {Element} password - The password input element.
+ * @param {Element} confirmPassword - The confirm password input element.
+ * @param {Element} msgBox - The message box element to show if the passwords do not match.
+ * @param {Array} actualisedUsers - The array of actual users.
+ * 
+ * @author: Robin
+ */
 async function putNewUser(password, confirmPassword, msgBox, actualisedUsers) {
     if (password.value === confirmPassword.value) {
         await putData('users', actualisedUsers);
         document.getElementById('registerContainer').classList.add('d-none');
+        let msgBoxSignedUp = document.getElementById('msgBoxSignedUp');
         msgBoxSignedUp.classList.remove('d-none');
         setTimeout(function () {
             window.location.href = 'login.html?msg=Registrierung erfolgreich';
@@ -245,20 +301,9 @@ async function putNewUser(password, confirmPassword, msgBox, actualisedUsers) {
         let msgBoxText = document.getElementById('msgBoxText');
         msgBoxText.innerHTML = `Ups! your password don't match.`;
         msgBox.classList.remove('d-none');
-        await setTimeout(function () {
+        await new Promise(resolve => setTimeout(() => {
             msgBox.classList.add('d-none');
-        }, 2000);
-    }
-}
-
-function login() {
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    let user = users.find(user => user.email === email.value && user.password === password.value);
-    if (user) {
-        window.location.href = 'summary.html';
-    } else {
-        msgBox.classList.remove('d-none');
-        msgBox.innerHTML = 'Login fehlgeschlagen';
+            resolve();
+        }, 2000));
     }
 }
